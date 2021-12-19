b610d863e969eb11a3d7760e78c6b2067681d843
Stefan Denker <Stefan@dn-kr.de>
2021-12-19 21:52:39 +0100

    initial version of calendar_throughput

---
+++ appointments/calender_throughput.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import tzlocal
+import requests
+import datetime
+from icalendar import Alarm, Calendar, Event, vDDDTypes
+
+def create_alarm(time: vDDDTypes):
+   alarmtime = tzlocal.get_localzone().localize(datetime.datetime( time.dt.year, time.dt.month, time.dt.day, 19,0,tzinfo=None) - datetime.timedelta(days=1))
+   alarm = Alarm()
+   alarm.add("ACTION", "DISPLAY")
+   alarm.add("TRIGGER", alarmtime)
+   return alarm
+
+
+r = requests.get(
+    "https://www.blauetonne-schlauetonne.de/abfuhrkalender/2022/pfinztal-woeschbach-kleinsteinbach-285.ics");
+
+calendar = Calendar.from_ical(r.content);
+for event in calendar.walk('vevent'):
+    del event['DTEND']
+    event.subcomponents = [ sc for sc in event.subcomponents if type(sc) != 'icalendar.cal.Alarm' ] # delete all alarms
+    event.add_component(create_alarm(event['DTSTART']))
+    print(type(event['DTSTART'].dt))
+
+print(calendar.to_ical())
+
